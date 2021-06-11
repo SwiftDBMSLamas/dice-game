5ea3b93ca70f7b95b4a67d17f1450440258a7325
SwiftDBMSLamas <67307011+SwiftDBMSLamas@users.noreply.github.com>
2021-06-10 19:22:10 -0700

    push updates to dice game

--- scripts/dice.js
+++ scripts/dice.js
@@ -4,26 +4,45 @@ author: Allan Aranzaso
 
 */
 
+/* 
+    todo: add animation
+    add 6 images (dice)
+*/
+//gather document elements
 const btnNewGame        = document.getElementById('btn-newGame');
 const btnRollDice       = document.getElementById('btn-rollDice');
+const btnPlayAgain      = document.getElementById('btn-playAgain');
+const btnClosePopUp     = document.getElementById('close-pop-up');
 const currentDate       = document.getElementById("dateTime");
 const playerName        = document.getElementById('playerName');
 const playerScore       = document.getElementById('playerScore');
+const playerFinalScore  = document.getElementById('playerFinalScore');
+const compFinalScore    = document.getElementById('compFinalScore');
 const compScore         = document.getElementById('compScore');
 const prevPage          = document.getElementById("go-back");
+const popup             = document.getElementById('gameOver-pop-up');
+const popupWinner       = document.getElementById('gameOver-winner-h2');
 const timeNow           = new Date();
+const diceDelay         = 1500;
+const maxVal            = 6;
+const defaultScore      = 0;
+const lastRound         = 3;
 
 currentDate.innerHTML += `${timeNow.toDateString()}`;
+// hides the popup until game is over
+popup.style.display = 'none';
+btnRollDice.style.cursor = "default";
+
 //todo: refactor code to remove global variables
 let gameRound = 0;
-const maxVal = 6;
-let nameOfPlayerPrompt;
 let humanPlayer;
 let computerPlayer;
-let btnRollDiceClicked = false;
 let computerTurn;
 let playerTurn = true;
-let gameOver = false;
+
+let totalScore = 0;
+let compTotalScore = 0;
+let turnInterval;
 
 class Dice {
     constructor(){
@@ -37,87 +56,158 @@ class Player {
         this.name = name;
     }
     rollDice( firstRoll, secondRoll ){
-        let score;
-        let totalScore = 0;
-        playerScore.innerHTML = "";
-        if(playerTurn){
-            if( firstRoll.value === 1 || secondRoll.value === 1) {
-                score = 0;
-            }else if( firstRoll.value === secondRoll.value ) {
-                score = (firstRoll.value + secondRoll.value) * 2;
-            }else {
-                score = firstRoll.value + secondRoll.value;
-            }
-            playerScore.innerHTML += `${score}`;
-            gameRound++;
-            if(gameRound === 3){
-                computerTurn = true;
-                gameRound = 0;
-                playerTurn = false;
-            }
-            //todo: add scores together at the end
-            totalScore += score;
-            console.log(totalScore);
+        
+        let score = getScore(firstRoll, secondRoll);
+
+        playerScore.textContent = score;
+
+        totalScore += score;
+        playerFinalScore.textContent = totalScore;
+
+        playerTurn = false;
+        computerTurn = true;
+        if(gameRound === 2){
+            totalScore = defaultScore;
         }
+        console.log(`${this.name}\'s roll was: ${firstRoll.value}, ${secondRoll.value} for a total of: ${score}`);
     }
 }
 
 class Computer {
     rollDice( firstRoll, secondRoll ){
-        let score;
-        compScore.innerHTML = "";
-        if(computerTurn){
-            if( firstRoll.value === 1 || secondRoll.value === 1) {
-                score = 0;
-            }else if( firstRoll.value === secondRoll.value ) {
-                score = (firstRoll.value + secondRoll.value) * 2;
-            }else {
-                score = firstRoll.value + secondRoll.value;
-            }
-            compScore.innerHTML += `  ${score}`;
-            gameRound++;
-            if(gameRound === 3){
-                computerTurn = false;
-                gameOver = true;
-            }
+
+        let score = getScore(firstRoll, secondRoll);
+
+        //display current rolled dice score
+        compScore.textContent = score;
+
+        //add and display the total score for the game so far
+        compTotalScore += score;
+        compFinalScore.textContent = compTotalScore;
+        //end turn
+        gameRound++;
+        computerTurn = false;
+        playerTurn = true;
+
+        if(gameRound === lastRound){
+            playerTurn = false;
+            getWinner();
+            compTotalScore = defaultScore;
         }
+        console.log(`Computer\'s roll was: ${firstRoll.value}, ${secondRoll.value} for a total of: ${score}`);
     }
 }
 
-btnNewGame.addEventListener('click', function(evt){
-    nameOfPlayerPrompt = window.prompt("Enter your name");
+/*
+    computes the score for the pair of dice rolled
+    firstRoll - the value of the first die rolled, between 1-6 randomly
+    secondRoll - the value of the second die rolled, between 1-6 randomly
+    Returns the score for that round
+*/
+function getScore( firstRoll, secondRoll ){
+    let score;
+    let roll1    = firstRoll.value;
+    let roll2    = secondRoll.value;
+    //todo: add a result if it's a tie
+    if( roll1 === 1 || roll2 === 1){
+        score = defaultScore;
+    }else if( roll1 === roll2 ){
+        score = (roll1 + roll2) * 2;
+    }else{
+        score = roll1 + roll2;
+    }
+    return score;
+}
+
+function getWinner() {
+    popup.style.display = 'block';
+    if(compFinalScore.textContent > playerFinalScore.textContent){
+        popupWinner.textContent = "Computer wins!";
+    }else{
+        popupWinner.textContent = `${playerName.textContent} wins!`;
+    }
+    btnRollDice.disabled = true;
+};
+
+btnNewGame.addEventListener('click', function(){
+    let nameOfPlayerPrompt = window.prompt("Please enter your name");
     
+    if(nameOfPlayerPrompt.length === 0){
+        nameOfPlayerPrompt = window.prompt('Field cannot be empty');
+    }
+
     playerName.innerHTML = nameOfPlayerPrompt;
     humanPlayer = new Player(nameOfPlayerPrompt);
     computerPlayer = new Computer();
 
     btnNewGame.disabled = true;
-
-    if(btnRollDiceClicked){
-        btnNewGame.disabled = false;
+    
+    btnRollDice.disabled = false;
+    
+    if(btnRollDice.disabled == false){
+        btnRollDice.style.cursor = "pointer";
+        btnNewGame.style.cursor = "default";
+    }
+    //reset the game when user clicks the button
+    if(btnNewGame.textContent === "Reset Game"){
+        resetTheGame();
     }
 });
 
 btnRollDice.addEventListener('click', function(){
-    let firstDie = new Dice();
-    let secondDie = new Dice();
-    let thirdDie = new Dice();
-    let fourthDie = new Dice();
-
+    // if for any reason the obj is undefined.. alert the user
     if(humanPlayer === undefined){
-        //todo: disable btn until new game is clicked
         alert('Please click new game first!');
     }
-
-    btnRollDiceClicked = true;
-    if(btnRollDiceClicked){
-        btnNewGame.disabled = false;
-    }
+    // change button text to reset game
+    btnNewGame.disabled = false;
+    btnNewGame.textContent = "Reset Game";
+    btnNewGame.style.cursor = "pointer";
     
-    humanPlayer.rollDice(firstDie, secondDie);
-    computerPlayer.rollDice(thirdDie, fourthDie);
-    //todo: automaticaly roll the dice three times, call function to compare winner, display winner with try again button
+    takeTurns();
 });
+
+btnPlayAgain.addEventListener('click', function(){
+    popup.style.display = 'none';
+    resetTheGame();
+
+});
+
+btnClosePopUp.addEventListener('click', function(){
+    popup.style.display = 'none';
+    clearInterval(turnInterval);
+});
+
+
+function takeTurns(){
+    
+    turnInterval = setInterval(() => {
+        
+        let firstDie = new Dice();
+        let secondDie = new Dice();
+        let thirdDie = new Dice();
+        let fourthDie = new Dice();
+
+        if(playerTurn){
+            humanPlayer.rollDice(firstDie, secondDie);
+        }else if(computerTurn){
+            computerPlayer.rollDice(thirdDie, fourthDie);
+        }
+    }, diceDelay);
+};
+
+// Reset the game to default values
+function resetTheGame(){
+    gameRound = 0;
+    playerFinalScore.textContent = defaultScore;
+    compFinalScore.textContent = defaultScore;
+    playerScore.textContent = defaultScore;
+    compScore.textContent = defaultScore;
+    playerTurn = true;
+    btnNewGame.textContent = "New Game";
+
+    clearInterval(turnInterval);
+};
 
 prevPage.addEventListener('click', function(){
     if(history != undefined){
